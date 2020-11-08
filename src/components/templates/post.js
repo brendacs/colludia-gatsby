97ee5d331aede5e7bda0d152505037d6789e3514
brendacs <brenda.zhang2@gmail.com>
2020-11-08 14:12:42 -0800

    Image paths fix, organize content folder, update post template

--- src/components/templates/post.js
+++ src/components/templates/post.js
@@ -8,38 +8,38 @@ export default function Template({
   const { markdownRemark } = data // data.markdownRemark holds your post data
   const { frontmatter, html } = markdownRemark
   return (
-    <main class="post-main">
-      <section class="post-page">
+    <main className="post-main">
+      <section className="post-page">
         <header>
-          <div class="post-page-image-container">
-            <img alt="post header image from game" class="post-page-image" src="{{ site.baseurl }}/images/headers/{{ page.image }}" />
+          <div className="post-page-image-container">
+            <img alt="post header image from game" className="post-page-image" src={require(`../../images/headers/${frontmatter.image}`)} />
           </div>
         </header>
-        <div class="article-wrapper">
-          <div class="share-buttons vertical">
-            <a href="https://twitter.com/intent/tweet?text=Check%20out%20this%20article%20on%20%40Colludia%21&url=https://colludia.com{{ page.url }}" name="share on twitter"><img alt="twitter logo" src="{{ site.baseurl }}/images/twitter.svg" /></a>
-            <a href="https://facebook.com/sharer.php?u=https://colludia.com{{ page.url }}" name="share on facebook"><img alt="facebook logo" src="{{ site.baseurl }}/images/facebook.svg" /></a>
-            <div class="share-link-wrapper">
-              <div class="tooltip hide">Copied!</div>
-              <a class="share-link" name="copy link to article to share"><img alt="link" src="{{ site.baseurl }}/images/link.svg" /></a>
+        <div className="article-wrapper">
+          <div className="share-buttons vertical">
+            <a href="https://twitter.com/intent/tweet?text=Check%20out%20this%20article%20on%20%40Colludia%21&url=https://colludia.com{{ page.url }}" name="share on twitter"><img alt="twitter logo" src={require('../../images/twitter.svg')} /></a>
+            <a href="https://facebook.com/sharer.php?u=https://colludia.com{{ page.url }}" name="share on facebook"><img alt="facebook logo" src={require('../../images/facebook.svg')} /></a>
+            <div className="share-link-wrapper">
+              <div className="tooltip hide">Copied!</div>
+              <a className="share-link" name="copy link to article to share"><img alt="link" src={require('../../images/link.svg')} /></a>
             </div>
-            <a class="jump-comments" href="#disqus_thread" name="jump to comments"><img alt="conment speech bubble" src="{{ site.baseurl }}/images/comment.svg" /></a>
+            <a className="jump-comments" href="#disqus_thread" name="jump to comments"><img alt="conment speech bubble" src={require('../../images/comment.svg')} /></a>
           </div>
-          <div class="article">
+          <div className="article">
             <article>
-              <div class="post-page-info">
-                <h1 class="post-page-title">{frontmatter.title}</h1>
-                <div class="post-page-details">
-                  <p><a href="{{ site.baseurl }}/author/{{ page.author-url }}" class="animated-link">{frontmatter.author}</a></p>
+              <div className="post-page-info">
+                <h1 className="post-page-title">{frontmatter.title}</h1>
+                <div className="post-page-details">
+                  <p><a href={`../../content/authors/${frontmatter.authorUrl}`} className="animated-link">{frontmatter.author}</a></p>
                   <p>{frontmatter.date}</p>
-                  {/* <p class="minute-estimate">{{ page.content | number_of_words | times: 2 | divided_by: 500 | plus: 1 }} minute read</p> */}
+                  {/* <p className="minute-estimate">{{ page.content | number_of_words | times: 2 | divided_by: 500 | plus: 1 }} minute read</p> */}
                 </div>
-                <div class="share-buttons horizontal">
+                <div className="share-buttons horizontal">
                   <a href="https://twitter.com/intent/tweet?text=Check%20out%20this%20article%20on%20Colludia%21&url=https://colludia.com{{ page.url }}" name="share on twitter"><img alt="twitter logo" src="{{ site.baseurl }}/images/twitter.svg" /></a>
                   <a href="https://facebook.com/sharer.php?u=https://colludia.com{{ page.url }}" name="share on facebook"><img alt="facebook logo" src="{{ site.baseurl }}/images/facebook.svg" /></a>
-                  <div class="share-link-wrapper">
-                    <div class="tooltip hide">Copied!</div>
-                    <a class="share-link" name="copy link to share"><img alt="link" src="{{ site.baseurl }}/images/link.svg" /></a>
+                  <div className="share-link-wrapper">
+                    <div className="tooltip hide">Copied!</div>
+                    <a className="share-link" name="copy link to share"><img alt="link" src="{{ site.baseurl }}/images/link.svg" /></a>
                   </div>
                 </div>
               </div>
@@ -51,7 +51,7 @@ export default function Template({
                 className="blog-post-content"
                 dangerouslySetInnerHTML={{ __html: html }}
               />
-              <div class="tags">
+              <div className="tags">
                 {/* {% for category in page.categories %}
                   {% include tag.html name=category %}
                 {% endfor %}
@@ -60,18 +60,18 @@ export default function Template({
                 {% endfor %} */}
               </div>
             </article>
-            <p class="sponsor-blurb">Help us pay our writers and maintain our high-quality website and writing by supporting us on <a href="https://patreon.com/colludia">Patreon</a> or buying us a coffee on <a href="https://ko-fi.com/colludia">Ko-fi</a>. To sponsor or advertise with us, visit <a href="{{site.baseurl}}/advertise">Advertise</a>.</p>
+            <p className="sponsor-blurb">Help us pay our writers and maintain our high-quality website and writing by supporting us on <a href="https://patreon.com/colludia">Patreon</a> or buying us a coffee on <a href="https://ko-fi.com/colludia">Ko-fi</a>. To sponsor or advertise with us, visit <a href="{{site.baseurl}}/advertise">Advertise</a>.</p>
             <div id="disqus_thread"></div>
           </div>
         </div>
       </section>
-      <input type="text" value="https://colludia.com{{ page.url }}" class="share-link-text invisible-input" autocorrect="off" spellcheck="false" readonly />
+      <input type="text" value="https://colludia.com{{ page.url }}" className="share-link-text invisible-input" autocorrect="off" spellcheck="false" readonly />
       {frontmatter.video &&
-        <div class="post-video-iframe" id="youtube">
-          <div class="play">
-            <div class="triangle"></div>
+        <div className="post-video-iframe" id="youtube">
+          <div className="play">
+            <div className="triangle"></div>
           </div>
-          <img class="post-video-iframe-image" src="{{ site.baseurl }}/images/headers/small/{{ page.image }}" />
+          <img className="post-video-iframe-image" src="{{ site.baseurl }}/images/headers/small/{{ page.image }}" />
         </div>
       }
     </main>
