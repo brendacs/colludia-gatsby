90ecbefc209d23c9d0bbc5fb2a86db76ad608ddc
brendacs <brenda.zhang2@gmail.com>
2020-11-29 16:35:45 -0800

    Implement search

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -6,6 +6,7 @@ import DropdownNav from "./dropdown-nav"
 
 const Header = ({ siteTitle, search }) => {
   const [showDropdownNav, setShowDropdownNav] = useState(false)
+  const [showSearch, setShowSearch] = useState(false)
 
   // Below is code to hide dropdown nav on scroll, but not using this UX for now
 
@@ -50,7 +51,14 @@ const Header = ({ siteTitle, search }) => {
 
         <div className="main-nav-actions">
           {search && (
-            <button className="search-btn action" name="search">
+            <button
+              className="search-btn action"
+              name="search"
+              onClick={() => {
+                setShowSearch(!showSearch)
+                setShowDropdownNav(false)
+              }}
+            >
               <img alt="search" src={require("../../images/search.svg")} />
             </button>
           )}
@@ -67,8 +75,14 @@ const Header = ({ siteTitle, search }) => {
             className="ham-menu-wrapper action"
             role="button"
             tabIndex={0}
-            onClick={() => setShowDropdownNav(!showDropdownNav)}
-            onKeyDown={() => setShowDropdownNav(!showDropdownNav)}
+            onClick={() => {
+              setShowDropdownNav(!showDropdownNav)
+              setShowSearch(false)
+            }}
+            onKeyDown={() => {
+              setShowDropdownNav(!showDropdownNav)
+              setShowSearch(false)
+            }}
           >
             <img
               alt="hamburger menu"
@@ -80,7 +94,7 @@ const Header = ({ siteTitle, search }) => {
       </nav>
       <div className="search-wrapper">
         <input
-          className="search hide"
+          className={`search ${showSearch ? "" : "hide"}`}
           type="search"
           placeholder="Search by title, author, or article type e.g. review"
           spellCheck="false"
