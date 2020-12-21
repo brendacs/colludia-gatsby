66faf98027a33a1fdc167bc550165d10941e96b9
brendacs <brenda.zhang2@gmail.com>
2020-12-20 17:32:47 -0800

    Add netlify identity widget

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,9 +2,13 @@ import React from "react"
 import SEO from "../components/seo"
 import ArticleList from "../components/layouts/article-list/article-list"
 import { featuredPost } from "../data/featured-posts"
+import { Helmet } from "react-helmet"
 
 const IndexPage = () => (
   <>
+    <Helmet>
+      <script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
+    </Helmet>
     <SEO title="Home" page="latest" />
     <ArticleList featuredPost={featuredPost.latest} page="latest" />
   </>
