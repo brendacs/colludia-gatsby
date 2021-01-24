b132e7b0f1aaa1c230cab114552c5ce615c03602
brendacs <brenda.zhang2@gmail.com>
2021-01-24 15:20:48 -0800

    Update featured post and data format

--- src/pages/our-picks.js
+++ src/pages/our-picks.js
@@ -1,7 +1,7 @@
 import React from "react"
 import SEO from "../components/seo"
 import ArticleList from "../components/layouts/article-list/article-list"
-import { featuredPost } from "../data/featured-posts"
+import featuredPost from "../data/featured-posts.json"
 
 const PicksPage = () => (
   <>
--- src/pages/reviews/short.js
+++ src/pages/reviews/short.js
@@ -1,13 +1,14 @@
 import React from "react"
 import SEO from "../../components/seo"
 import ArticleList from "../../components/layouts/article-list/article-list"
-import { featuredPost } from "../../data/featured-posts"
+import featuredPost from "../../data/featured-posts.json"
 
 const ShortPage = () => (
   <>
+    {console.log(featuredPost)}
     <SEO title="Short &amp; Sweet Reviews" page="review" />
     <ArticleList
-      featuredPost={featuredPost.review}
+      featuredPost={featuredPost.short}
       page="review"
       subpage="short"
       sortable
